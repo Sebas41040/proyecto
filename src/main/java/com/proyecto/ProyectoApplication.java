d9ab030d8add954eaf25e37cacf2dab2cd0ce2b9
Sebas <sebas41040@gmail.com>
2021-11-29 19:08:49 -0300

    Se añade una excepcion para que no de error en el host

--- src/main/java/com/proyecto/ProyectoApplication.java
+++ src/main/java/com/proyecto/ProyectoApplication.java
@@ -2,8 +2,9 @@ package com.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
