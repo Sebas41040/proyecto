8308ea6dccf8bd8037c400e096cabaff4bb60aca
Sebas <sebas41040@gmail.com>
2021-11-30 22:36:28 -0300

    Se prueba nuevamente

--- src/main/java/com/proyecto/utils/ProyectoApplication.java
+++ src/main/java/com/proyecto/utils/ProyectoApplication.java
@@ -2,11 +2,9 @@ package com.proyecto.utils;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 
 @SpringBootApplication()
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
