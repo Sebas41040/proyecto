5392c89d23f0f115fa09030526d4607cb252143a
Sebas <sebas41040@gmail.com>
2021-12-01 14:03:35 -0300

    Se añade el ComponentScan

--- src/main/java/com/proyecto/ProyectoApplication.java
+++ src/main/java/com/proyecto/ProyectoApplication.java
@@ -2,8 +2,10 @@ package com.proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"controller"})
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
